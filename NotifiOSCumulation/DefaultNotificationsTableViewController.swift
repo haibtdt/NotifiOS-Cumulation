30a4f942483719bdd6d22086f3534485cb170035
Bui Hai <haibtdt@gmail.com>
2015-09-30 06:54:26 +0700

    [UPDATE] built-in notification view controller to allow deleting an archived notification

--- NotifiOSCumulation/DefaultNotificationsTableViewController.swift
+++ NotifiOSCumulation/DefaultNotificationsTableViewController.swift
@@ -63,8 +63,9 @@ public class DefaultNotificationsTableViewController: UITableViewController {
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+//         Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+         self.navigationItem.rightBarButtonItem = self.editButtonItem()
+
     }
 
 
@@ -96,7 +97,7 @@ public class DefaultNotificationsTableViewController: UITableViewController {
         cell.textLabel!.text = notif.title
         let isNotificationRead = notif.read!.boolValue
         cell.backgroundColor = isNotificationRead ? UIColor.whiteColor() : UIColor.lightGrayColor()
-            
+        
         
         
 
@@ -118,49 +119,35 @@ public class DefaultNotificationsTableViewController: UITableViewController {
         
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
 
-    /*
+
+
     // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    override public func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
         if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            
+            notificationCumulationCenter?.remove(allNotifcations[indexPath.row]) { (error : NSError?) -> () in
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    
+                    if error == nil {
+                        
+                        self.allNotifications_.removeAtIndex(indexPath.row)
+                        self.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+                        
+                        
+                    } else {
+                        
+                        
+                    }
+                    
+                })
+                
+            }
+            
+        }
+        
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
