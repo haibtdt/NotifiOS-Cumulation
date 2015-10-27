2edf9d46af2443ffd686ce5dd3a2dcdea5576ce6
Nibbana <buihai19872015@gmail.com>
2015-10-27 17:03:26 +0700

    [UPDATE] implement mark all as read method

--- NotifiOSCumulation/NotifiOSCumulationCenter.swift
+++ NotifiOSCumulation/NotifiOSCumulationCenter.swift
@@ -188,58 +188,13 @@ public class NotifiOSCumulationCenter {
     public func markAllAsRead (callback: (NSError?)->()) {
         
         
+        markAsRead(allNotifications!, callback: callback)
         
         
     }
 
     
 }
-
-class NCPersistenceStackSetup : NSObject{
-    
-    var storePath_ : NSURL
-    var storePath : NSURL {
-        
-        return storePath_
-        
-    }
-    
-
-    
-    init (storePath : NSURL) {
-        
-        storePath_ = storePath
-        
-    }
-    
-    
-    var context_ : NSManagedObjectContext! = nil
-    var context : NSManagedObjectContext! {
-     
-            return context_
-        
-    }
-    
-    
-    func setUpContext () {
-        
-        if context_ == nil {
-            
-            let thisBundle = NSBundle(forClass: NCPersistenceStackSetup.classForCoder())
-            var managedModel_ : NSManagedObjectModel! = nil
-            var persistenceCoordinator_ : NSPersistentStoreCoordinator! = nil
-            managedModel_ = NSManagedObjectModel(contentsOfURL: thisBundle.URLForResource("Model", withExtension: "momd")!)
-            persistenceCoordinator_ = NSPersistentStoreCoordinator(managedObjectModel: managedModel_)
-            try! persistenceCoordinator_.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: storePath, options: [NSInferMappingModelAutomaticallyOption:true])
-            context_ = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
-            context_?.persistentStoreCoordinator = persistenceCoordinator_
-            
-        }
-        
-    }
-    
-}
-
 
 
 
